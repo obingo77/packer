b492ce5d2eb5aa078f7aa5279209fb77a6745fd0
Ubuntu <ubuntu@ip-172-31-45-74.ec2.internal>
2021-09-13 03:18:22 +0000

    modify terraform output.tf

--- instances/main.tf
+++ instances/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "sg_22_80" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-08f530f8d4a33cb40"
+  ami                         = "ami-08f530f8d4a33cb40" //  TO DO
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
   vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
